dbdc1d072749aa277ff9564e6520d99201eb4859
pepe_witch <skpepe134@gmail.com>
2019-04-16 18:28:37 +0700

    :sparkles: using average of 3 circles as a prediction

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 from crop import getCropRect, findCircle
 from summary import getInsideCircle, getOutsideCircles
-from os.path import join
+from os.path import join, dirname
 from os import getcwd
 from sys import argv
 from skimage.io import imread
+import pickle
+import numpy as np
 
 def getFilepath():
     if(len(argv) == 1):
@@ -23,16 +25,20 @@ def getData(img):
         inside = getInsideCircle(crop, circle)
         output.append([
             inside['avg_r'],
-            inside['std_r'],
             inside['avg_g'],
-            inside['std_g'],
             inside['avg_b'],
-            inside['std_b'],
+            outside['avg_r'],
+            outside['avg_g'],
+            outside['avg_b'],
         ])
+    return output
 
 def main():
     filepath = getFilepath()
     img = imread(filepath)
-    
+    data = getData(img)
+    model = pickle.load(open(join(dirname(__file__),'model.p'), 'rb'))
+    print(np.average(model.predict(data)))
+
 if __name__ == '__main__':
     main()
