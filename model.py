b6105f1c348138dde5c7c6ab3302819b5b654247
pepe_witch <skpepe134@gmail.com>
2019-04-20 00:33:41 +0700

    :sparkles: convert list to np array

--- model.py
+++ model.py
@@ -4,4 +4,6 @@ coef = np.array([-0.21741104,  0.24249119, -0.01685429,  0.1394792 , -0.24525309
 intercept = -7.605807281061505
 
 def predict(data):
+    if(type(data) != np.ndarray):
+        data = np.array(data)
     return data.dot(coef) + intercept
