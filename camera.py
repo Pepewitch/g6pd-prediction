a4947f5b89aad35b233fc676313f78f203fb1122
pepe_witch <skpepe134@gmail.com>
2019-05-06 11:02:43 +0700

    test rgb

--- camera.py
+++ camera.py
@@ -39,7 +39,7 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
  
 # capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
